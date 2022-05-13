e60bee9952a2731f58dd869d40439cbc16ecb8eb
sullayang(杨金伟) <sullayang@futunn.com>
2022-05-13 15:04:49 +0800

    区块验证

--- src/modal/block.ts
+++ src/modal/block.ts
@@ -6,12 +6,35 @@ export default class Block {
   constructor(public index: number, public hash: string, public previousHash: string,
     public timestamp: number, public data: string) {
   }
+  // 验证区块结构是否有效
+  isVaidStructure() {
+    return typeof this.index === 'number'
+      && typeof this.hash === 'string'
+      && typeof this.previousHash === 'string'
+      && typeof this.timestamp === 'number'
+      && typeof this.data === 'string'
+  }
+  // 验证hash
+  isVaildHash(): boolean {
+    return this.hash === Block.calculateHash(this.index, this.previousHash, this.timestamp, this.data);
+  }
+  // 验证前一个区块
+  isVaildPreviousBlock(previousBlock: Block): boolean {
+    if (previousBlock.index + 1 !== this.index) return false;
+    if (previousBlock.hash !== this.previousHash) return false;
+    return true;
+  }
+  // 验证区块是否有效
+  isVaild(previousBlock: Block): boolean {
+    return this.isVaidStructure() && this.isVaildHash && this.isVaildPreviousBlock(previousBlock);
+  }
   // 第一个block
-  static firstBlock = new Block(0, 'd074bb799bdd3ca7c19a14ceb6f5a7f451decfce2199d9fc2992b0fddc527e40', null, 1652347167272, 'first block');
+  static readonly firstBlock: Readonly<Block> = new Block(0, 'd074bb799bdd3ca7c19a14ceb6f5a7f451decfce2199d9fc2992b0fddc527e40', null, 1652347167272, 'first block');
   // 计算hash
   static calculateHash(index: number, previousHash: string, timestamp: number, data: string): string {
     return cryptoJs.SHA256(index + previousHash + timestamp + data).toString();
   }
+
   // 生成block
   static createNextBlock(data: string): Block {
     const previousBlock = blockchain.getLatestBlock();
--- src/modal/blockChain.ts
+++ src/modal/blockChain.ts
@@ -5,7 +5,21 @@ class BlockChain {
   public list: Block[] = [Block.firstBlock];
   // 获取最后一个block
   getLatestBlock(): Block {
-    return this.list[this.list.length - 1]
+    return this?.list[this.list.length - 1]
+  }
+  isValid(): boolean {
+    if (!this?.list.length) return false;
+    let firstBlock = this.list[0];
+    if (firstBlock.index !== Block.firstBlock.index) return false;
+    if (firstBlock.previousHash !== Block.firstBlock.previousHash) return false;
+    if (firstBlock.timestamp !== Block.firstBlock.timestamp) return false;
+    if (firstBlock.data !== Block.firstBlock.data) return false;
+    if (firstBlock.hash !== Block.firstBlock.hash) return false;
+
+    for (let i = 1; i < this.list.length; i++) {
+      if (!this.list[i].isVaild(this.list[i - 1])) return false;
+    }
+    return true;
   }
 }
 
